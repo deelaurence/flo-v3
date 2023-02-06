5253229710214bed1f9cf26ba564249cee7c36f5
deelaurence <laurie143pelumi@gmail.com>
2023-02-06 22:45:55 +0100

    kodetech fix

--- src/components/KODETECH.jsx
+++ src/components/KODETECH.jsx
@@ -287,9 +287,9 @@ const KODETECH = ({ locationProps }) => {
                         <div className='mt-14 font-semibold text-[1.5rem] mb-6'>
                             <h3>Task Flows.</h3>
                         </div>
-                        <div className='flex flex-col gap-4 text-opaque'>
+                        <div className='flex flex-col gap-8 text-opaque font-medium'>
                             <div>
-                                <h5 className='mb-5'>Sign Up/log In task flow.</h5>
+                                <h5 className='mb-5'>Sign Up/Log In task flow.</h5>
                                 <img onClick={handleEnlargeLandscape} src={userFlow1} alt="" />
                             </div>
                             <div>
